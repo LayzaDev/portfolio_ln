5cf51cc1c5bc8dc993fc4ac6b177ef5ef376ca53
LayzaDev <layzanauanedev@gmail.com>
2023-11-20 21:26:07 -0300

    implementando javascript

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-let nome = document.querySelector("#inputName");
-let email = document.querySelector("#inputEmail");
-let msg = document.querySelector("#textArea");
+function openWhatsApp() {
+  let name = document.querySelector("#inputName").value;
+  let occupation = document.querySelector("#inputOccupation").value;
+  let message = document.querySelector("#messageField").value;
+  let phoneNumber = "+5534988273701";
+
+  let enviar = `${name} - ${occupation} \n${message}`;
+
+  let whatsappLink = `https://wa.me/${phoneNumber}/?text=${encodeURIComponent(
+    enviar
+  )}`;
+
+  window.open(whatsappLink);
+}
+
+document.getElementById("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  openWhatsApp();
+});
